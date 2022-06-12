8cd09709bafddf90f175f82e23753092cdf738a2
jopegi <josevi_1976@hotmail.com>
2022-06-12 09:41:32 +0200

    Añadimos los recursos públicos

--- index.php
+++ index.php
@@ -25,15 +25,80 @@ echo "</pre>";
 
 ?>
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+
+  <!-- Style -->
+  <link rel="stylesheet" href="css/style.css">
+
+  <title>Employee Monthly Rating - Chips&Bits Inc.</title>
 </head>
+
 <body>
-    
+
+
+  <div class="content">
+
+    <div class="container">
+      <h2 class="mb-5">Employee Monthly Rating - Chips&Bits Inc.</h2>
+
+
+      <div class="table-responsive">
+
+        <table class="table table-striped custom-table">
+          <thead>
+            <tr>
+
+              <th scope="col">&nbsp;</th>
+              <th scope="col">Name</th>
+              <th scope="col">Skills</th>
+              <th scope="col">Rating</th>
+              <th scope="col">&nbsp;</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr scope='row'>
+              <td> <img src='img/photo/hop1.jpg' width='50'> </td>
+              <td>Hopper, Xavier P.</td>
+              <td> Frontend <small class='d-block'>Sed diam lorem, auctor quis, tristique ac, eleifen...</small> </td>
+              <td>
+                <div class='rating_container'> <a href='ratingUpdate.php?id=1&rating=1'><img src='img/star_up.png'
+                      width='10'></a><a href='ratingUpdate.php?id=1&rating=2'><img src='img/star_up.png'
+                      width='10'></a><a href='ratingUpdate.php?id=1&rating=3'><img src='img/star_up.png'
+                      width='10'></a><a href='ratingUpdate.php?id=1&rating=4'><img src='img/star_down.png'
+                      width='10'></a><a href='ratingUpdate.php?id=1&rating=5'><img src='img/star_down.png'
+                      width='10'></a> </div>
+              </td>
+              <td><a href='card.php?id=1' class='more'>+Info</a></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+
+    </div>
+
+  </div>
+
+
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script>
 </body>
+
 </html>
